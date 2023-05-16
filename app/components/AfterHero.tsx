4d0ab1789d3ca4eecf023498a7d54a0ff28878cf
Darrell-Rafa <drafa0609@gmail.com>
2023-05-16 19:58:46 +0700

    first commit

---
+++ app/components/AfterHero.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Countdown from "react-countdown";
+
+function AfterHero() {
+  function renderer({
+    days,
+    hours,
+    minutes,
+    completed,
+  }: {
+    days: any;
+    hours: any;
+    minutes: any;
+    seconds: any;
+    completed: any;
+  }) {
+    return completed ? (
+      <span>Udeh nikah</span>
+    ) : (
+      <span>
+        {days} hari {hours} jam {minutes} menit
+      </span>
+    );
+  }
+
+  return (
+    <section id="after-hero">
+      <div className="min-h-screen bg-[url('/images/afterHero/bg.jpg')] bg-cover bg-center text-white relative py-28 flex items-end">
+        <div className="relative py-8 px-10 w-full">
+          <div
+            className="bg-[#00000021] rounded-tr-[3.125rem] rounded-bl-[3.125rem] absolute inset-x-4 inset-y-0 z-10"
+            style={{ backdropFilter: "blur(6px)" }}
+          />
+          <div className="relative z-20">
+            <p className="text-sm font-light">WEDDING INVITATION</p>
+            <h1 className="text-3xl font-light mt-2 mb-4">Akbar & Retha</h1>
+            <div className="grid grid-cols-2 border-y border-y-white text-center text-sm font-light">
+              <p className="leading-[1.85] py-2 px-2 border-r border-r-white">
+                SAVE THE DATE
+                <br />
+                Sabtu, 3 Mei 2023
+              </p>
+              <p className="py-2 px-2 leading-[1.85]">
+                COUNTDOWN
+                <br />
+                <Countdown date={new Date("2023-06-03")} renderer={renderer} />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AfterHero;
---
+++ app/components/Hero.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+import { RxEnvelopeOpen } from "react-icons/rx";
+
+function Hero() {
+  return (
+    <section id="hero">
+      <div className="min-h-screen bg-[url('/images/hero/bg.jpg')] bg-cover bg-center text-white relative">
+        <div className="absolute inset-0 bg-black/60 z-10" />
+        <div className="flex flex-col justify-between py-28 px-12 text-center z-20 relative h-screen">
+          <div>
+            <p className="font-extralight text-[0.75rem]">The Wedding of</p>
+            <h1 className="text-4xl font-light mt-2">Akbar & Retha</h1>
+            <p className="font-extralight text-[0.8rem] mt-4">
+              Sabtu, 3 Mei 2023
+            </p>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-[0.8rem]">Mercure Hotel Bandung City Centre</p>
+            <p className="text-[0.8rem]">
+              Jl. Lengkong Besar No.8, Cikawao, Kec. Lengkong, Kota Bandung,
+              Jawa Barat
+            </p>
+            <a
+              href="#after-hero"
+              onClick={() => {
+                document.body.style.overflow = "auto";
+              }}
+              className="font-bold text-sm bg-[#ffffff36] border border-[#bdb08f8c] rounded-lg flex items-center gap-2 px-6 py-3 mt-6 hover:scale-90 ease-linear duration-[0.2s]"
+            >
+              <RxEnvelopeOpen />
+              <span>Buka Undangan</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
